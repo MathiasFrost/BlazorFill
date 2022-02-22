b35912d06311ce92ea77a9b0493d3f199116a76b
Lakavi <lakavi@protonmail.com>
2022-02-22 16:17:16 +0100

    Remove .idea

---
+++ BlazorFill.Components/Dropdown/DropdownModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorFill.Components.Dropdown;
+
+public struct DropdownModel
+{
+    public string Content;
+    public bool Html;
+    public Action Click;
+    public IEnumerable<DropdownModel> Children;
+}
